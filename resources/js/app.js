b89acdffd8049d56af89a3a964f2483215d299f6
Nderi12 <nderikamau1212@gmail.com>
2023-01-11 19:31:09 +0300

    Minor change

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import AppComponent from './components/App.vue'
 // import router from './components/router/index.js';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+// axios.defaults.baseURL = 'http://localhost:8000/api/';
 
 axios.defaults.headers['Authorization'] = `Bearer ${sessionStorage.getItem('token')}`;
 
